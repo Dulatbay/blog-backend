6161282be9d90281937d7dbde97633ca82dff802
Akhan <93190613+Dulatbay@users.noreply.github.com>
2024-04-09 23:05:39 +0500

    Update BlogBackendApplication.java

--- src/main/java/techpark/net/blogbackend/BlogBackendApplication.java
+++ src/main/java/techpark/net/blogbackend/BlogBackendApplication.java
@@ -3,6 +3,7 @@ package techpark.net.blogbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BlogBackendApplication {
     public static void main(String[] args) {
